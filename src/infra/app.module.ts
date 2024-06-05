94bf5a3be4a18e334f27d01e951c06a1c9b7b3f2
Lucas Martins <l10smts@gmail.com>
2024-06-04 22:25:34 -0300

    chore: config vitest

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
-import { AppController } from '@/app.controller'
-import { AppService } from '@/app.service'
-
-import { envSchema } from './infra/env/env'
-import { EnvModule } from './infra/env/env.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
