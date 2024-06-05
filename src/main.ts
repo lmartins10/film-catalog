e189a944f9bcef66748c63501d355ff15bb2e0b5
Lucas Martins <l10smts@gmail.com>
2024-06-04 22:16:07 -0300

    chore: env configs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+  const app = await NestFactory.create(AppModule, {
+    // logger: false
+  })
+
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
+
+  await app.listen(port)
 }
 bootstrap()
