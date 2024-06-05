513a0bba62816e34ef08321bb332be66879e49e3
Lucas Martins <l10smts@gmail.com>
2024-06-04 23:28:38 -0300

    fix: UserRole schema.prisma

--- prisma/migrations/20240605022503_create_users/migration.sql
+++ prisma/migrations/20240605022503_create_users/migration.sql
@@ -1,9 +1,13 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'CLIENT');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'ADMIN',
     "inactivated_at" TIMESTAMP(3),
     "password_reset_token" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
